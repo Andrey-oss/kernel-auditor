7fb4d3dedcc7dae285d3c567da9e24a8b84257af
Andrey-oss <62800423+Andrey-oss@users.noreply.github.com>
2025-05-20 20:02:21 +0300

    Delete scripts directory

--- scripts/del_syswide.sh
+++
@@ -1,3 +0,0 @@
-sudo systemctl disable --now kernel_auditor.service
-sudo rm -rf /usr/share/kernel_auditor
-sudo rm -rf /etc/systemd/system/kernel_auditor.service
--- scripts/make_syswide.sh
+++
@@ -1,3 +0,0 @@
-sudo cp -r $PWD /usr/share
-sudo cp -r others/kernel_auditor.service /etc/systemd/system
-sudo systemctl enable --now kernel_auditor.service
