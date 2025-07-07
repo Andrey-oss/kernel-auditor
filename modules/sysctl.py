68595ea52cb6ddea42f6d0dc5a2629f4723d2030
Andrey-oss <62800423+Andrey-oss@users.noreply.github.com>
2025-07-07 14:02:39 +0300

    [FIX]: Fixed f-string for 3.10/3.11 python version else code won't work

--- modules/sysctl.py
+++ modules/sysctl.py
@@ -32,11 +32,20 @@ def set_sysctl_param(data: dict) -> dict:
     try:
         cmd = f"sysctl {data['name']}={data['value']}"
     except KeyError:
-        return {'status': 'error', 'message': 'Sysctl param name or value not found in request!'}
+        return {
+            'status': 'error',
+            'message': 'Sysctl param name or value not found in request!'
+        }
 
     error_msg = run_cmd(cmd) # I don't know how to fix sysctl output into the console, but let it be
 
     if error_msg:
-        return {'status': 'error', 'message': error_msg}
+        return {
+            'status': 'error',
+            'message': error_msg
+        }
 
-    return {'status': 'ok', 'message': f'Parameter {data['name']} was changed successfully!'}
+    return {
+        'status': 'ok',
+        'message': f"Parameter {data['name']} was changed successfully!"
+    }
